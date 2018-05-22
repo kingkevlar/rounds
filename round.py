d4a8b344131435f0966a921de556af61f3124248
kingkevlar <fartherandhigher@yahoo.com>
2018-05-22 01:32:31 -0400

    Create round.py

---
+++ round.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import math
+
+def round_pain(real_efforts, pining):
+	assert(len(real_efforts)==len(pining))
+	pain = 0
+	zipped_efforts = zip(real_efforts, pining)
+	for effort in zipped_efforts:
+		for e in effort:
+			pain += e**2
+	return pain
+
+
+real_effort = [0.3, 0.05, 0.18]
+pining 		= [0.2, 0.25, 0.02]
+print round_pain(real_effort, pining)
